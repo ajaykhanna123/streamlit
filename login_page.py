ffd925a788d1396506282320a75d3e28d89d564f
ajaykhanna123 <32953636+ajaykhanna123@users.noreply.github.com>
2023-09-11 23:12:41 +0530

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -1,6 +1,9 @@
 import streamlit as st
-import pandas as pd
 from sentence_transformers import SentenceTransformer, util
+import openai
+import re
+import pandas as pd
+
 
 # Define a function to filter out columns with PHI data
 def filter_phi_columns(columns):
@@ -93,6 +96,7 @@ def main():
             # Add text boxes for question and output
                 question = st.text_input("Question", placeholder="Enter your question here")
                 if st.button("Submit"):
+                    answer=ask_csv(df, 'Show me the comparison of total claims for each ICD10code using graph', True)
                     # Here, you can process the question and provide an output based on the data in the CSV
                     # For demonstration purposes, we'll simply display the question.
                     st.write("Question:", question)
@@ -107,6 +111,107 @@ def main():
     st.sidebar.write("- Handle complex data processing.")
     st.sidebar.write("- Advanced natural language processing.")
     st.sidebar.write("- Real-time updates or database connections.")
+
+
+class generate_code():
+
+  api_key=''
+  #api_key=''
+  openai.api_key=api_key
+
+  def __init__(self,df):
+    self.df=df
+    self.num_rows=len(df)
+    self.num_columns=len(df.columns)
+    self.data_types=df.dtypes
+
+  def query(self,payload):
+    response=openai.Completion.create(
+      engine="text-davinci-002",
+      prompt=payload["inputs"],
+      temperature=payload["temperature"],
+      max_tokens=payload["max_new_tokens"])
+
+    return response['choices'][0]['text'].strip()
+
+
+  def prompt_generation(self,question):
+    columns=self.df.columns
+    col_list=','.join(columns)
+    prompt=f'''Write a python code on pandas dataframe df with columns:{col_list}.
+    The pandas dataframe already exists with the following schema:
+    {self.df.dtypes}
+    The code assumes the dataframe df while the below code.
+    The code should be able to display results for the following user query:
+    {question}
+    The code should also print the final results using the print statement
+    'Do not add comments in the code'
+    The code must be executed when passed to exec function
+    'Always assume that dataframe df already exists'
+    'The code should import the necessary modules'
+    'Try to avoid using loops while writing the code if possible'
+'''
+    return prompt
+
+  @staticmethod
+  def clean_code(code):
+    read_patt=re.compile(r'pd\.read\_csv\([A-Za-z0-9]*\.csv\)')
+    code=re.sub(read_patt,'',code)
+    matplotlib_patt=re.compile(r"\%matplotlib inline")
+    code=re.sub(matplotlib_patt,'',code)
+    return code
+
+
+  def generate(self,
+            original_prompt, temperature=0.9, max_new_tokens=256, top_p=0.95, repetition_penalty=1.0
+        ):
+
+            prompt=original_prompt
+            generate_kwargs = {
+                "temperature":temperature,
+                "max_new_tokens":max_new_tokens,
+                "top_p":top_p,
+                "repetition_penalty":repetition_penalty,
+                "do_sample":True,
+                "seed":42
+            }
+
+            suffix=''
+
+            prompt_dict={"inputs":prompt,**generate_kwargs}
+            code_response=self.query(prompt_dict)
+            #for i in range(5):
+            prompt_dict={"inputs":prompt,**generate_kwargs}
+            code_response=self.query(prompt_dict)
+            prompt=code_response
+
+            match = re.search(
+              "(```python)(.*)(```)",
+               prompt.replace(prompt+suffix, ""),
+                re.DOTALL | re.MULTILINE,
+                            )
+            # if match:
+            #   break
+
+
+            final_code=prompt.replace(original_prompt+suffix, "")
+            #final_code=final_code.split("\'''")
+            #final_code=final_code.split("You can use the below code to get the answer:")
+
+            return final_code
+
+def ask_csv(df,questions,show_code=False):
+  code_generator=generate_code(df)
+  prompt=code_generator.prompt_generation(question=questions)
+  code=code_generator.generate(prompt)
+  code=code_generator.clean_code(code)
+  if show_code:
+      print(code)
+  else:
+      pass
+  for statements in code.split('\n'):
+    exec(statements)
+
 
 if __name__ == "__main__":
     main()
